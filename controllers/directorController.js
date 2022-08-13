52189087efda00a93f17562a412202ceb2780897
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-14 01:28:01 +0900

    create a director boiler plate controller

---
+++ controllers/directorController.js
@@ -0,0 +1,42 @@
+const Director = require("../models/director");
+
+
+// Display list of all directors
+exports.director_list = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director List");
+}
+
+// Display a detail page of a specific director
+exports.director_detail = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director Detail");
+}
+
+// Display director create form on GET
+exports.director_create_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director create GET");
+}
+
+// Handle director create on POST
+exports.director_create_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director create POST");
+}
+
+// Display director delete form on GET
+exports.director_delete_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director delete GET");
+}
+
+// Handle director delete form on POST
+exports.director_delete_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director delete POST");
+}
+
+// Display director update form on GET
+exports.director_update_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director update GET");
+}
+
+// Handle director update form on POST
+exports.director_update_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Director update POST");
+}
