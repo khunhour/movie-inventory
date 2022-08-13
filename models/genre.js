9c8a15f0374ea31bdf2a7ff7a600b28b282aae59
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-13 21:42:34 +0900

    created a genre model schema

--- models/genre.js
+++ models/genre.js
@@ -1,4 +1,14 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
-let GenreSchema = new 
+let GenreSchema = new Schema({
+	name: { type: String, minLength: 3, maxLength: 100 },
+});
+
+// Virtual for genre's URL
+GenreSchema.virtual("url").get(function () {
+	return "/genre/" + this._id;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
