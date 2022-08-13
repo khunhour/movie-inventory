f46409c80d3f39ee2e59e0df94e29b5b5c49d0a7
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-14 01:24:27 +0900

    create a boilerplate controller for genre

---
+++ controllers/genreController.js
@@ -0,0 +1,42 @@
+const Genre = require("../models/genre");
+
+
+// Display list of all genres
+exports.genre_list = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre List");
+}
+
+// Display a detail page of a specific genre
+exports.genre_detail = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre Detail");
+}
+
+// Display genre create form on GET
+exports.genre_create_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre create GET");
+}
+
+// Handle genre create on POST
+exports.genre_create_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre create POST");
+}
+
+// Display genre delete form on GET
+exports.genre_delete_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre delete GET");
+}
+
+// Handle genre delete form on POST
+exports.genre_delete_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre delete POST");
+}
+
+// Display Genre update form on GET
+exports.genre_update_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre update GET");
+}
+
+// Handle Genre update form on POST
+exports.genre_update_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Genre update POST");
+}
