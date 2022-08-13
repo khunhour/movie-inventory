c0b9f1830b365329b4c88222f396b7d305713baf
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-14 01:29:21 +0900

    create a movie boiler plate controller

---
+++ controllers/movieController.js
@@ -0,0 +1,42 @@
+
+const Movie = require("../models/movie");
+
+// Display list of all movies
+exports.movie_list = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie List");
+}
+
+// Display a detail page of a specific movie
+exports.movie_detail = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie Detail");
+}
+
+// Display movie create form on GET
+exports.movie_create_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie create GET");
+}
+
+// Handle movie create on POST
+exports.movie_create_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie create POST");
+}
+
+// Display movie delete form on GET
+exports.movie_delete_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie delete GET");
+}
+
+// Handle movie delete form on POST
+exports.movie_delete_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie delete POST");
+}
+
+// Display movie update form on GET
+exports.movie_update_get = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie update GET");
+}
+
+// Handle movie update form on POST
+exports.movie_update_post = (req, res) =>{
+  res.send("NOT IMPLEMENTED: Movie update POST");
+}
