2335ccbaa413021fb7d093f962eff28c37f33a5f
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-14 18:00:40 +0900

    fix routing typo

--- routes/directorRouter.js
+++ routes/directorRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 const director_controller = require("../controllers/directorController");
 
--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 const genre_controller = require("../controllers/genreController");
 
