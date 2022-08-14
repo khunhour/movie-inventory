39072502c506a34770a11f0f503c8f30ef6ca109
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-14 18:34:23 +0900

    remove date of birth and death from director model

--- models/director.js
+++ models/director.js
@@ -4,8 +4,6 @@ let Schema = mongoose.Schema;
 let DirectorSchema = new Schema({
 	first_name: { type: String, required: true, maxLength: 100 },
 	family_name: { type: String, required: true, maxLength: 100 },
-	date_of_birth: { type: Date },
-	date_of_death: { type: Date },
 });
 
 // Virtual for director's full name
